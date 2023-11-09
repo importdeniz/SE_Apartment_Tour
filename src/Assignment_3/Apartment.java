94d8690127950efb4bb37b15b2cb5ddd702d91f1
witti02 <116462359+witti02@users.noreply.github.com>
2023-11-09 20:11:39 +0100

    test

--- src/Assignment_3/Apartment.java
+++ src/Assignment_3/Apartment.java
@@ -22,6 +22,6 @@ public class Apartment {
         return entered;
     }
     public void addRoom(Room room){
-        rooms.add(room);     // test
+        rooms.add(room);     // test asdfasdf
     }
 }
