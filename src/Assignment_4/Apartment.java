5cfea0c9bb4fbd3c91fd860188aa878a1f0f674d
importdeniz <grundmandeniz@gmail.com>
2023-11-09 23:49:16 +0100

    Initial commit

--- src/Assignment_4/Apartment.java
+++ src/Assignment_4/Apartment.java
@@ -4,30 +4,27 @@ import java.util.*;
 
 public class Apartment {
     Set<Room> rooms = new HashSet<>();
-    private boolean entered;
-    public Apartment(){
-        this.entered = false;
-    }
+    public boolean entered = false;
 
     public void enter(){
         this.entered = true;
     }
-    public void leave(){
-        this.entered = false;
-    }
+//    public void leave(){
+//        this.entered = false;
+//    }
 
-    public boolean isEntered(){
-        return entered;
-    }
+//    public boolean isEntered(){
+//        return entered;
+//    }
     public void addRoom(Room room){
         rooms.add(room);
     }
 
 
-    public void getRooms() {    //print alle Räume in Apartment
-        for (Room i : rooms) {
-            System.out.println(i.getName());
-        }
-    }
+//    public void getRooms() {    //print alle Räume in Apartment
+//        for (Room i : rooms) {
+//            System.out.println(i.getName());
+//        }
+//    }
 }
 
